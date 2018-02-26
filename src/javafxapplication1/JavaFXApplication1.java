2e86d30653638ea03d7da7797b2b801c1d2ccfa1
natariyz <evgeniy.vabishevich.99@mail.ru>
2018-02-26 19:56:36 +0300

    TopInterface
    Begin to create the top interface

--- src/javafxapplication1/JavaFXApplication1.java
+++ src/javafxapplication1/JavaFXApplication1.java
@@ -15,13 +15,21 @@ import javafx.stage.Stage;
 import javafx.application.Platform;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Priority;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.MenuBar;
+import javafx.geometry.HPos;
 
 /**
  *
@@ -31,32 +39,62 @@ public class JavaFXApplication1 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+    
+    GridPane TopInterface = new GridPane();
         
-        BorderPane root = new BorderPane();       
-        Scene scene = new Scene(root, 900, 500);
-        
-        MenuBar menuBar = new MenuBar();
-        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
-        root.setTop(menuBar);
-        
-        Menu fileMenu = new Menu("File");
-        MenuItem newMenuItem = new MenuItem("New");
-        MenuItem loadMenuItem = new MenuItem("Load");
-        MenuItem saveMenuItem = new MenuItem("Save");
-        MenuItem save_asMenuItem = new MenuItem("Save as");
-        MenuItem exitMenuItem = new MenuItem("Exit");
-        exitMenuItem.setOnAction(actionEvent -> Platform.exit());
-        
-        fileMenu.getItems().addAll(newMenuItem,loadMenuItem,
-                saveMenuItem,save_asMenuItem,exitMenuItem);
-        
-        menuBar.getMenus().addAll(fileMenu);
-        
-        primaryStage.setTitle("MyPaint");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    ColumnConstraints [] columnsArray = new ColumnConstraints[15];
+    for (int i = 0; i < columnsArray.length; i++){
+        columnsArray[i] = new ColumnConstraints();
     }
-
+    
+    for (int i = 0; i < columnsArray.length; i++) {
+        TopInterface.getColumnConstraints().addAll(columnsArray[i]);
+        if(i < 10) columnsArray[i].setPercentWidth(5);
+        else columnsArray[i].setPercentWidth(10);
+    }
+    
+    MenuBar menuBar = new MenuBar();
+    Menu fileMenu = new Menu("File");
+    MenuItem exitMenuItem = new MenuItem("Exit");
+    exitMenuItem.setOnAction(actionEvent -> Platform.exit());
+    
+    fileMenu.getItems().addAll(exitMenuItem);
+    menuBar.getMenus().addAll(fileMenu);
+    
+    Button [] buttonArray = new Button[25];         //11-20 colors, 20-25(copy,paste,cut,back,forward)
+    for (int i = 0; i < buttonArray.length; i++){
+        buttonArray[i] = new Button();
+    }
+    
+    for(int i = 0; i < 5; i++){                 //Placement of the main instruments
+        TopInterface.add(buttonArray[i], i, 1);
+        TopInterface.add(buttonArray[i+5], i, 2);
+    }
+    
+    for(int i = 5; i < 10; i++){                //Placement of palette
+        TopInterface.add(buttonArray[i+5], i, 1);
+        TopInterface.add(buttonArray[i+10], i, 2);
+    }
+    
+    for(int i = 20; i < 25; i++){
+        TopInterface.add(buttonArray[i], i - 10, 1);
+    }
+    
+    TopInterface.add(menuBar, 0, 0);
+    GridPane.setConstraints(menuBar, 0, 0, 15, 1);
+    GridPane.setConstraints(buttonArray[20], 10, 1, 1, 2);//Increasing the size of some functions
+    GridPane.setConstraints(buttonArray[21], 11, 1, 1, 2);
+    GridPane.setConstraints(buttonArray[22], 12, 1, 1, 2);
+    GridPane.setConstraints(buttonArray[23], 13, 1, 1, 2);
+    GridPane.setConstraints(buttonArray[24], 14, 1, 1, 2);
+    
+    Scene mainScene = new Scene(TopInterface, 500, 500);
+    primaryStage.setScene(mainScene);
+    primaryStage.show();
+    
+    
+    
+    }
     /**
      * @param args the command line arguments
      */
